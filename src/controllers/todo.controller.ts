3c0a7b0a50ac9fb36c8c0bc2a6ada026b8772fc4
j0ec0de <joelanto386@gmail.com>
2025-12-06 00:02:16 +0530

    Added remaining crud operations

--- src/controllers/todo.controller.ts
+++ src/controllers/todo.controller.ts
@@ -17,5 +17,45 @@ export async function createTodo(req: Request, res: Response) {
     }
 };
 
+export async function getTodo(req: Request, res: Response) {
+    try {
+        const todo = await prisma.todo.findMany();
+        res.json(todo);
+    } catch (err: any) {
+        return res.status(500).json({ error: err.message })
+    }
+};
 
+export async function updateTodo(req: Request, res: Response) {
+    try {
+        const id = Number(req.params.id);
+        const { title, completed } = req.body;
+
+        const todo = await prisma.todo.update({
+            where: { id },
+            data: {
+                title: title,
+                completed: completed
+            }
+        });
+
+        return res.json(todo);
+
+    } catch (err: any) {
+        return res.status(500).json({ error: err.message });
+    }
+};
+
+export async function deleteTodo(req: Request, res: Response) {
+    try {
+        const id = Number(req.params.id);
+        const todo = await prisma.todo.delete({
+            where: { id }
+        })
+
+        return res.json({ message: "Deleted" });
+    } catch (err: any) {
+        return res.status(500).json({ error: err.message });
+    }
+}
 
--- src/routes/todo.routes.ts
+++ src/routes/todo.routes.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 
-import { createTodo } from '../controllers/todo.controller';
+import { createTodo, deleteTodo, getTodo, updateTodo } from '../controllers/todo.controller';
 
 import { validateBody } from '../middlewares/validateBody';
 
 const router = express.Router();
 
 router.post('/', validateBody({ title: "string" }), createTodo);
-
+router.get('/', getTodo);
+router.put('/:id', validateBody({ title: "string", completed: "boolean" }) ,updateTodo);
+router.delete('/:id', deleteTodo);
 export default router;
 
